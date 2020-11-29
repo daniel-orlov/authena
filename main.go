a38dacaefb8128447ac66f1fe3dc06bc9b6471e5
daniel-orlov <daniel.orlov@ctemplar.com>
2020-11-29 13:52:59 +0300

    basic structure defined

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"authena/user"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+)
+
+//4 REST routes:
+//
+// 1 - returns a pair of Access-Refresh tokens for the user
+//	   with GUID specified in request
+//
+// 2 - executes Refresh operation on a pair of Access-Refresh
+//     token
+//
+// 3 - deletes Refresh token from the db
+//
+// 4 - deletes all Refresh tokens from the db for a specified user
+//
+//
+var router = gin.Default()
+
+func main() {
+	router.POST("/login", login)
+	log.Fatal(router.Run(os.Getenv("PORT")))
+}
+
+//func login
